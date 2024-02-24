f7a38fbf229e0e0f259fb0ecf6cd1248e84642ad
Jahnavi24387 <kallurujahnavi@gmail.com>
2024-02-24 11:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
+@app.route('/recommendationfromopenai', methods=['GET'])
+def getRecommendations():
+   query = request.args.get('ask')
+   print(query)
+   llm_model=LLMModel()
+   llm_items = llm_model.get_llm_items(query)
+   if query:
+       print('Request for hello page received with name=%s' % query)
+       return llm_items
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return "No results found"
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
